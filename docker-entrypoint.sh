c5caf2d27489602017cd946a9323734853fe11c0
K. Burak Özdemir <kburakozdemir@users.noreply.github.com>
2024-09-05 14:52:33 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 if [ "$1" = 'init' ]; then
     composer create-project drupal-tome/tome-project /var/www/tome --stability dev --no-interaction
-    drush tome:init
+    vendor/bin/drush tome:init
 elif [ "$1" = 'runserver' ]; then
     echo "Use this URL to login as admin once the server is started:"
-    drush uli -l 0.0.0.0:8888
+    vendor/bin/drush uli -l 0.0.0.0:8888
     echo
-    drush runserver 0.0.0.0:8888
+    vendor/bin/drush runserver 0.0.0.0:8888
 else
     exec "$@"
 fi
